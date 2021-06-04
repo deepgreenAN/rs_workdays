dfed089bce9a4a90362aacd2a5ba7badbbaf6bde
deepgreenAN <asami73dgreen63@gmail.com>
2021-06-04 13:45:30 +0900

    modify doc comment of lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,13 @@
+//! # 営業日・営業時間を取得・抽出
+
+/// 祝日・休日曜日・営業時間などの設定
 pub mod global;
+
+/// 営業日の取得・チェック
 pub mod workdays;
+
+/// 営業時間内かチェック・営業時間のDuration演算
 pub mod intraday;
+
+/// 営業時間内のデータの抽出
 pub mod extract;
