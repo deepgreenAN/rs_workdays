39d81c2301d4a7a9e45838730c7abba61905cbdd
deepgreenAN <asami73dgreen63@gmail.com>
2021-12-03 17:30:49 +0900

    re-exportを追加

--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,32 @@
 
 /// 祝日・休日曜日・営業時間などの設定
 pub mod global;
+pub use global::{
+    set_holidays_csvs, 
+    set_intraday_borders, 
+    set_one_holiday_weekday_set, 
+    set_range_holidays
+};
 
 /// 営業日の取得・チェック
 pub mod workdays;
+pub use workdays::*;
 
 /// 営業時間内かチェック・営業時間のDuration演算
 pub mod intraday;
+pub use intraday::*;
 
 /// 営業時間内のデータの抽出
 pub mod extract;
+pub use extract::*;
 
 /// エラー
 pub mod error;
+pub use error::Error;
 
 /// スクレイピング
 #[cfg(not(features="wasm"))]
 pub mod scrape_holidays;
+
+#[cfg(not(features="wasm"))]
+pub use scrape_holidays::*;
